9907fc074b92a072a75e8f99afe6d0b2914c3960
Haji Fernandez <hjefernandez@gmail.com>
2021-05-29 11:41:08 +0800

    fix: register function to display validation messages

--- controller/userAuth.js
+++ controller/userAuth.js
@@ -2,49 +2,47 @@ const express = require("express");
 const createService = require("../service/createUser");
 const loginService = require("../service/retrieveUser");
 const bcrypt = require("bcryptjs");
+const withErrors = require("../utils/withErrors");
+const EJS_INFO = require("../constants/ejs");
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-
   const result = await loginService(email);
 
   if (!result.length) {
-    res.render("login",{
-      ejsModalDisplay : "block",
-      ejsPageTitle : "Register",
-      ejsHeadBackground : "bg-danger",
-      ejsMessageTitle : `Email Does not Exist `,
-      ejsServerMessage : `${email} does not exist, Please check carefully. If you do not have account yet, Click register`,
-      ejsRedirectPage : `/`,
-      ejsMessageButton: "Close"
-
-    })
+    res.render("login", {
+      ejsModalDisplay: "block",
+      ejsPageTitle: "Register",
+      ejsHeadBackground: "bg-danger",
+      ejsMessageTitle: `Email Does not Exist `,
+      ejsServerMessage: `${email} does not exist, Please check carefully. If you do not have account yet, Click register`,
+      ejsRedirectPage: `/`,
+      ejsMessageButton: "Close",
+    });
   }
 
-  if(result){
-
-    const [data] = result 
+  if (result) {
+    const [data] = result;
 
     const match = await bcrypt.compare(password, data.password);
-    
-        if(match) {
-            res.render("cart", {
-                ejsOrders: [],
-                ejsName: data.name,
-              });
-        }
-        else{
-          res.render("login",{
-            ejsModalDisplay : "block",
-            ejsPageTitle : "Register",
-            ejsHeadBackground : "bg-danger",
-            ejsMessageTitle : `Wrong Password `,
-            ejsServerMessage : `You have entered wrong password, Please check before submitting`,
-            ejsRedirectPage : `/`,
-            ejsMessageButton: "Close"
-          })
-        }   
+
+    if (match) {
+      res.render("cart", {
+        ejsOrders: [],
+        ejsName: data.name,
+      });
+    } else {
+      res.render("login", {
+        ejsModalDisplay: "block",
+        ejsPageTitle: "Register",
+        ejsHeadBackground: "bg-danger",
+        ejsMessageTitle: `Wrong Password `,
+        ejsServerMessage: `You have entered wrong password, Please check before submitting`,
+        ejsRedirectPage: `/`,
+        ejsMessageButton: "Close",
+      });
+    }
   }
 };
 
@@ -53,18 +51,22 @@ const register = async (req, res) => {
 
   const result = await createService(name, email, mobile, password);
 
-  if (result) {
-    res.render("serverMessage",{
-      ejsPageTitle : "Register Success",
-      ejsHeadBackground : "bg-success",
-      ejsMessageTitle : "Registered Successfully",
-      ejsServerMessage : "Welcome You have Successfully Registered to Eclick, Please click button to Login",
-      ejsRedirectPage : "/",
-      ejsMessageButton: "Go to Login Page"
+  if (typeof result === "boolean" && result === true) {
+    res.render("register", {
+      ejsModalDisplay: "block",
+      ejsPageTitle: "Register Success",
+      ejsHeadBackground: "bg-success",
+      ejsMessageTitle: "Registered Successfully",
+      ejsServerMessage:
+        "Welcome You have Successfully Registered to Eclick, Please click button to Login",
+      ejsRedirectPage: "/",
+      ejsMessageButton: "Go to Login Page",
     });
-    console.log("New user Created")
+    console.log("New user Created");
   } else {
-   console.log("user try to create but failed")
+    res.locals.errors = withErrors(result);
+    res.locals.oldValue = { name, email, mobile };
+    res.render("register", { ...EJS_INFO });
   }
 };
 
