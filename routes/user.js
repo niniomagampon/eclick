89a2e6b8f9e7be03ca4baba018a420e903afec23
osirisgenius <59709335+osirisgenius@users.noreply.github.com>
2021-05-30 23:01:51 +0800

    rename users file

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,9 @@
 const express = require("express")
 const userRoute = express.Router()
-<<<<<<< Updated upstream
 const accountController = require("../controller/account.controller")
 const EJS_INFO = require("../constants/ejs")
 
 userRoute.post("/", accountController.login)
-=======
-const accountService = require("../service/account.service");
-const EJS_INFO = require("../constants/ejs")
-
-userRoute.post("/", accountService.login)
->>>>>>> Stashed changes
 userRoute.get("/", (req, res) => {
     res.render("login", { ...EJS_INFO })
 })
@@ -18,10 +11,6 @@ userRoute.get("/", (req, res) => {
 userRoute.get("/register", (req, res) => {
     res.render("register", { ...EJS_INFO })
 })
-<<<<<<< Updated upstream
 userRoute.post("/register", accountController.register)
-=======
-userRoute.post("/register", accountService.register)
->>>>>>> Stashed changes
 
 module.exports = userRoute
