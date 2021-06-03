3a9423ed6b5f3e1047d48f57776b83cda16bc6ac
Ninio <ninomagampon@gmail.com>
2021-06-04 07:17:24 +0800

    use prettier

--- controller/page.controller.js
+++ controller/page.controller.js
@@ -3,52 +3,51 @@ const productService = require("../service/ProductService");
 const EJS_INFO = require("../constants/ejs");
 const loggedInSession = require("../utils/loginSession");
 
-let userName = "Guest"
-let logInOut = "Login"
+let userName = "Guest";
+let logInOut = "Login";
 
-const home = async (req, res) =>{
-    if (req.session.isLoggedIn) {
-        userName = req.session.username;
-        logInOut = "Logout";
-      }
-      const products = await productService.all()
+const home = async (req, res) => {
+  if (req.session.isLoggedIn) {
+    userName = req.session.username;
+    logInOut = "Logout";
+  }
 
-      res.render("customer/home", {
-        ...EJS_INFO,
-        userName,
-        logInOut,
-        products
-      });
-}
+  const products = await productService.all();
 
-const loginPage = async(req, res) =>{
-    loggedInSession(req);
-    res.render("customer/login", { ...EJS_INFO });
-}
+  res.render("customer/home", {
+    ...EJS_INFO,
+    userName,
+    logInOut,
+    products,
+  });
+};
 
-const registerPage = async(req, res) =>{
-    res.render("customer/register", {
-        ...EJS_INFO,
-      });
-}
+const loginPage = async (req, res) => {
+  loggedInSession(req);
+  res.render("customer/login", { ...EJS_INFO });
+};
 
-const contact = async(req, res) =>{
-    if (req.session.isLoggedIn) {
-        userName = req.session.username;
-        logInOut = "Logout";
-      }
-    
-      res.render("customer/contact", {
-        userName,
-        logInOut,
-      });
-}
+const registerPage = async (req, res) => {
+  res.render("customer/register", {
+    ...EJS_INFO,
+  });
+};
 
+const contact = async (req, res) => {
+  if (req.session.isLoggedIn) {
+    userName = req.session.username;
+    logInOut = "Logout";
+  }
 
+  res.render("customer/contact", {
+    userName,
+    logInOut,
+  });
+};
 
 module.exports = {
-    home,
-    registerPage,
-    contact,
-    loginPage
-}
+  home,
+  registerPage,
+  contact,
+  loginPage,
+};
