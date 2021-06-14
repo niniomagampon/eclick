c6b623aec29c5716eef60dfea18aca4f723ad539
osirisgenius <59709335+osirisgenius@users.noreply.github.com>
2021-06-15 02:12:57 +0800

    change sync to alter true try mariadb on heroku

--- configs/database.js
+++ configs/database.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("eclick", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
+// module.exports = new Sequelize("eclick", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+module.exports = new Sequelize("eclick", "jhlbdj3betw100xa", "v5jm81alh13uqt70", {
+  host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  dialect: "mariadb",
 });
