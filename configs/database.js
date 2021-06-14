82ae4981f0b7c38694994dc73d8f809e37d39b1b
osirisgenius <59709335+osirisgenius@users.noreply.github.com>
2021-06-15 02:25:03 +0800

    update database name

--- configs/database.js
+++ configs/database.js
@@ -5,7 +5,7 @@ const { Sequelize } = require("sequelize");
 //   dialect: "mysql",
 // });
 
-module.exports = new Sequelize("eclick", "jhlbdj3betw100xa", "v5jm81alh13uqt70", {
+module.exports = new Sequelize("ng9nwgqbnej550by", "jhlbdj3betw100xa", "v5jm81alh13uqt70", {
   host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect: "mariadb",
 });
