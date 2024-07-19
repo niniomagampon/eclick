54540cf06643e6482b0633070cfe658232d615ab
Niño Magampon <82831506+niniomagampon@users.noreply.github.com>
2024-07-19 13:13:19 +0800

    Update database.js
    update db password

--- configs/database.js
+++ configs/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("eclick", "root", "", {
+module.exports = new Sequelize("eclick", "root", "root", {
   host: "localhost",
   dialect: "mysql",
 }); 
